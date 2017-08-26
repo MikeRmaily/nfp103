6064927dfa67b1fa8a6c612eda065b8b44c61e66
RmailyMike <RmailyMike@gmail.com>
2017-08-26 15:04:17 +0300

    Merge origin/master
    Conflicts:
    	Server/build/classes/server/Server.class
    	Server/src/server/Server.java

--- Server/src/server/SACA/MultiSACAServeur.java
+++ Server/src/server/SACA/MultiSACAServeur.java
@@ -14,6 +14,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
 import client.MultiSACAClient;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.util.Set;
+
 /**
  *
  * @author Mike
@@ -21,6 +25,8 @@ import client.MultiSACAClient;
 
 public class MultiSACAServeur implements ActionListener  {  
   public static final int PORT = 8080;
+
+
   public static void main(String[] args)
       throws IOException {
          JFrame frame = new JFrame("JFrame Example");
@@ -29,6 +35,12 @@ public class MultiSACAServeur implements ActionListener  {
         JLabel label = new JLabel("Add client!");
         JButton button = new JButton();
         button.setText("Press me");
+        JTextField nomAvion = new JTextField("", 8);
+        JTextField XPosition = new JTextField("", 8);
+        JTextField YPosition = new JTextField("", 8);
+        panel.add(nomAvion);
+        panel.add(XPosition);
+        panel.add(YPosition);
         panel.add(label);
         panel.add(button);
         frame.add(panel);
@@ -48,9 +60,33 @@ public class MultiSACAServeur implements ActionListener  {
                     s = new ServerSocket(PORT);
                     System.out.println("Server Started");
                     MultiSACAClient MultiSACAClient = new MultiSACAClient();
-                    MultiSACAClient.AddClient();
+                    Thread curThread = MultiSACAClient.AddClientThread(nomAvion.getText());
+                   
                     socket = s.accept();
                     new SACAServer(socket);
+                       
+                 
+                     //   System.out.println(MultiSACAClient.threadCount());
+                   
+                        MultiSACAClient.setX(Integer.parseInt(XPosition.getText()));
+                        MultiSACAClient.setY(Integer.parseInt(YPosition.getText()));
+                       System.out.println(MultiSACAClient.tg1.activeCount());
+//                        System.out.println(getRootThreadGroup().activeCount());
+                      //  System.out.println(curThread.getName());
+                     //   System.out.println(curThread.getThreadGroup().getName());
+//                         System.out.println(getRootThreadGroup().getParent());
+//                           System.out.println(getAllThreadGroups().length);
+                        
+//                             Thread[] threads = MultiSACAClient.getAllThreads();
+//                            System.out.println("coun" + threads.length);
+//                            for (Thread t  : threads) {
+//                                   System.out.print("Thread name"  + t.getName());
+//                                }
+//                            Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+//                            Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
+//                            System.out.println("------------");
+//                            System.out.println("Length" + threadArray.length);
+//                             System.out.println("------------");
                 } catch (IOException ex) {
                      try {
                          socket.close();
@@ -73,6 +109,52 @@ public class MultiSACAServeur implements ActionListener  {
         });
   } 
 
+  
+                    static ThreadGroup rootThreadGroup = null;
+                   static ThreadGroup getRootThreadGroup( ) {
+                        if ( rootThreadGroup != null )
+                            return rootThreadGroup;
+                        ThreadGroup tg = Thread.currentThread( ).getThreadGroup( );
+                        ThreadGroup ptg;
+                        while ( (ptg = tg.getParent( )) != null )
+                            tg = ptg;
+                        return tg;
+                    }
+                   
+                static ThreadGroup[] getAllThreadGroups( ) {
+                 final ThreadGroup root = getRootThreadGroup( );
+              
+                 int nAlloc = root.activeGroupCount( );
+                 int n = 0;
+                 ThreadGroup[] groups;
+                 do {
+                     nAlloc *= 2;
+                     groups = new ThreadGroup[ nAlloc ];
+                     n = root.enumerate( groups, true );
+                 } while ( n == nAlloc );
+
+                 ThreadGroup[] allGroups = new ThreadGroup[n+1];
+                 allGroups[0] = root;
+                 System.arraycopy( groups, 0, allGroups, 1, n );
+                 return allGroups;
+             }
+                
+              static  Thread[] getAllThreads( ) {
+               // final ThreadGroup root = getRootThreadGroup( );
+                  final ThreadGroup root = MultiSACAClient.getTg1();
+                final ThreadMXBean thbean = ManagementFactory.getThreadMXBean( );
+                int nAlloc = thbean.getThreadCount( );
+                int n = 0;
+                Thread[] threads;
+                do {
+                    nAlloc *= 2;
+                    threads = new Thread[ nAlloc ];
+                    n = root.enumerate( threads, true );
+                } while ( n == nAlloc );
+                return java.util.Arrays.copyOf( threads, n );
+            }
+
+                
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
